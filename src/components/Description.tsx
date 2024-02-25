506b69d65b9c51d5398fd1f27b84f407dfe138bd
Brady <bradysuryasie@gmail.com>
2024-02-25 13:55:47 +1100

    nice

---
+++ src/components/Description.tsx
@@ -0,0 +1,41 @@
+import { useCollapse } from "react-collapsed";
+
+type Props = {
+  description: string;
+};
+
+const Description = (props: Props) => {
+  const { getCollapseProps, getToggleProps } = useCollapse();
+
+  const paragraphs = props.description.split("\n");
+
+  const firstParagraph = paragraphs[0];
+  const remainingParagraphs = paragraphs.slice(1);
+
+  console.log(remainingParagraphs);
+
+  return (
+    <>
+      <section>{firstParagraph}</section>
+      <br />
+      <section {...getCollapseProps()}>
+        {remainingParagraphs.map((paragraph) => {
+          return (
+            <>
+              <p>{paragraph}</p>
+              <br />
+            </>
+          );
+        })}
+      </section>
+      <a {...getToggleProps()} className="iv-expand-btn">
+        <span className="me-2">
+          <i className="fa-solid fa-plus"></i>
+        </span>
+        Show More
+      </a>
+    </>
+  );
+};
+
+export default Description;
